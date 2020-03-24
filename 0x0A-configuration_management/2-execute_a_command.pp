f0ccab8f8cb56ec9ee428ee8d41ec9a2b73f2551
pafuentess <pafuentess@unal.edu.co>
2020-03-24 09:12:17 -0500

    change to path

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'killmenow':
-  command  => 'pkill --signal SIGTERM killmenow',
-  path     => '/usr/bin',
+  command => 'pkill --signal SIGTERM killmenow',
+  path    => '/usr/bin',
 }
