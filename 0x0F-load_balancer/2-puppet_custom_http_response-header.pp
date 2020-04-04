761a79768763d1e7dba88963ab90fe2e6c4f0e97
pafuentess <pafuentess@unal.edu.co>
2020-04-03 21:16:00 -0500

    task adv

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,6 @@
+#documentation
+
+exec { 'configure HTTP':
+  command  => "apt-get update && apt-get -y install nginx && echo -e 'Holberton School' > /var/www/html/index.html && sed -i '/listen 80 default_server;/a rewrite ^/redirect_me https://www.youtube.com/watch?v=EgBJmlPo8Xw permanent;' /etc/nginx/sites-available/default && sed -i '22i add_header X-Served-By $hostname;' /etc/nginx/nginx.conf && service nginx start",
+  provider => 'shell',
+}
