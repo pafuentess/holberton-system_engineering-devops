629346696844646765c3195a955ed7887d2be5bb
pafuentess <pafuentess@unal.edu.co>
2020-05-04 12:29:28 -0500

    second commit

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" ... """
+
+
+import requests
+from sys import argv
+import csv
+import json
+
+if __name__ == "__main__":
+    if len(argv) == 2:
+        if int(argv[1]):
+            info = {"userId": argv[1]}
+            url = "https://jsonplaceholder.typicode.com/todos"
+            answer = requests.get(url, params=info).json()
+            ids = argv[1]
+            url = "https://jsonplaceholder.typicode.com/users/{}".format(ids)
+            name = requests.get(url).json().get("name")
+            columns = ['id', 'name', 'status', 'title']
+            to_dict = {}
+            dicts = []
+            for task in answer:
+                task_ok = task.get("completed")
+                value = [task.get("title"), task_ok, name]
+                dicts.append(dict(zip(columns, value)))
+            to_dict.update({"{}".format(ids):dicts})
+            with open("{}.json".format(ids), 'w') as f:
+                json.dump(to_dict, f)
+                    
---
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" ... """
+
+
+import requests
+from sys import argv
+import csv
+import json
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/todos"
+    answer = requests.get(url).json()
+    url = "https://jsonplaceholder.typicode.com/users"
+    users = requests.get(url).json()
+    ids = {}
+    for user in users:
+        ids.update({user.get("id"): user.get("username")})
+    columns = ['task', 'completed', 'username']
+    to_dict = {}
+    for task in answer:
+        user_id = task.get("UserId")
+        dicts = to_dict.get(user_id)
+        if dicts is None:
+            dicts = []
+        name = ids.get(user_id)
+        completed = task.get("completed")
+        values = [task.get("title"), completed, name]
+        dicts.append(dict(zip(columns, values)))
+        to_dict.update({user_id: dicts})
+    with open("todo_all_employees.json", 'w') as f:
+        json.dump(to_dict, f)
+                    
