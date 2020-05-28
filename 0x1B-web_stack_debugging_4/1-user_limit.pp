437e141359c724f45705156328bcead580ce9f2c
pafuentess <pafuentess@unal.edu.co>
2020-05-28 17:54:11 -0500

    first task

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+#....
+exec {'name_file_fixed':
+  path    => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command => "sed -i 's/4/1000/g' /etc/security/limits.conf && sed -i 's/5/1000/g' /etc/security/limits.conf"
+}
